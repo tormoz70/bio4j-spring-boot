1a8a109d4688135941c465a0b773448489ae3d37
bio <gpsdom@gmail.com>
2020-05-16 11:57:39 +0300

    ABean give for adoption to LinkedHashMap

--- t-model/src/main/java/ru/bio4j/spring/model/transport/ABean.java
+++ t-model/src/main/java/ru/bio4j/spring/model/transport/ABean.java
@@ -2,6 +2,7 @@ package ru.bio4j.spring.model.transport;
 
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
-public class ABean extends HashMap<String, Object> implements Serializable {
+public class ABean extends LinkedHashMap<String, Object> implements Serializable {
 }
