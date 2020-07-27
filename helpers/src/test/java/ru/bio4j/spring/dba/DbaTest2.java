1018dd9baf9cf6651fb6a2ec1c38d656ded7b3b1
ayrat <gpsdom@gmail.com>
2020-07-27 22:53:20 +0300

    wc

---
+++ helpers/src/test/java/ru/bio4j/spring/dba/DbaTest2.java
@@ -0,0 +1,49 @@
+package ru.bio4j.spring.dba;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.bio4j.spring.commons.types.Paramus;
+import ru.bio4j.spring.model.transport.ABean;
+import ru.bio4j.spring.model.transport.Param;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=DbaAutoConfiguration.class)
+@TestPropertySource(locations="classpath:test.properties")
+public class DbaTest2 {
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private DbaAdapter dbaAdapter;
+
+    @Test
+    public void doTest1() {
+        //DbaAdapter dbaAdapter = (DbaAdapter)applicationContext.getBean("dbaAdapter");
+        ABean d = dbaAdapter.loadFirstBean("rcard", (List<Param>) null, null, ABean.class);
+        Assert.assertTrue(d != null);
+    }
+    @Ignore
+    @Test
+    public void doTest2() {
+        //DbaAdapter dbaAdapter = (DbaAdapter)applicationContext.getBean("dbaAdapter");
+        List<Param> params = Paramus.createParams(
+            "sortord", "sum",
+            "rlocale", "ru",
+            "currId", 0,
+            "userOrgId", "244",
+            "userRole", "6",
+            "nationId", 0
+        );
+        ABean d = dbaAdapter.loadFirstBean("bios.curTopAll", params, null, ABean.class);
+        Assert.assertTrue(d != null);
+    }
+}
